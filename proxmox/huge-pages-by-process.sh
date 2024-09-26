1dc25c850092bd6c6b76ceb6b16227e63f0437c4
Henryk Paluch <henryk.paluch@pickering.cz>
2024-09-26 06:40:34 +0000

    huge-pages-by-process.sh requires gawk

--- proxmox/huge-pages-by-process.sh
+++ proxmox/huge-pages-by-process.sh
@@ -3,11 +3,18 @@
 # - modified version from: https://access.redhat.com/solutions/46111
 set -eu
 
+# requires 'gawk' - some Proxmox installs have 'mawk' which has no 'gensub' function.
+f=gawk
+which "$f" || {
+	echo "ERROR:  command '$f' not available" >&2
+	exit 1
+}
+
 # generate ps header only
 ps -fp 1 | head -1
 # list all THP by process (does NOT group listing by PID)
 # we filter out zero HugePages to avoid race condition when process quickly exits
-awk  '/AnonHugePages:[ \t]+[^0]/ { if($2>4){print FILENAME " " $0; system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", FILENAME))}}' \
+gawk  '/AnonHugePages:[ \t]+[^0]/ { if($2>4){print FILENAME " " $0; system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", FILENAME))}}' \
        	/proc/*/smaps
 exit 0
 
