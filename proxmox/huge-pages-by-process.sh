82da09552e875193af07e8869e3a43065a11b605
Henryk Paluch <henryk.paluch@pickering.cz>
2024-09-26 07:15:59 +0000

    Refactored - now it print sum of THP by process

--- proxmox/huge-pages-by-process.sh
+++ proxmox/huge-pages-by-process.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # huge-pages-by-process.sh - List Processes with Anonymous HugePages (typically KVM)
 # - modified version from: https://access.redhat.com/solutions/46111
+# - summing data from: https://stackoverflow.com/a/14916890
 set -eu
 
 # requires 'gawk' - some Proxmox installs have 'mawk' which has no 'gensub' function.
 f=gawk
-which "$f" || {
+which "$f" > /dev/null 2>&1 || {
 	echo "ERROR:  command '$f' not available" >&2
 	exit 1
 }
 
 # generate ps header only
 ps -fp 1 | head -1
-# list all THP by process (does NOT group listing by PID)
+# list all THP by process
 # we filter out zero HugePages to avoid race condition when process quickly exits
-gawk  '/AnonHugePages:[ \t]+[^0]/ { if($2>4){print FILENAME " " $0; system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", FILENAME))}}' \
-       	/proc/*/smaps
+gawk  '/AnonHugePages:[ \t]+[1-9]/ { if($2>4){ kb[FILENAME] += $2 } }
+END{ for (a in kb){
+	system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", a));
+	print "+-> AnonHugePages:",kb[a]/1024.0,"MB"
+     }
+}' /proc/*/smaps
 exit 0
 
