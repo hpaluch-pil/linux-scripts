a0dae8049e10a1d3e08ccd832da993040c3e57ee
Henryk Paluch <henryk.paluch@pickering.cz>
2024-09-26 07:32:56 +0000

    Adde fault and mem columns to ps output

--- proxmox/huge-pages-by-process.sh
+++ proxmox/huge-pages-by-process.sh
@@ -10,15 +10,16 @@ which "$f" > /dev/null 2>&1 || {
 	echo "ERROR:  command '$f' not available" >&2
 	exit 1
 }
+fmt="-o uid,pid,ppid,min_flt,maj_flt,vsz:8,rss:8,pmem,c,stime,tty,time,cmd"
 
 # generate ps header only
-ps -fp 1 | head -1
+ps -fp 1 $fmt | head -1
 # list all THP by process
 # we filter out zero HugePages to avoid race condition when process quickly exits
 gawk  '/AnonHugePages:[ \t]+[1-9]/ { if($2>4){ kb[FILENAME] += $2 } }
 END{ for (a in kb){
-	system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", a));
-	print "+-> AnonHugePages:",kb[a]/1024.0,"MB"
+	system("ps --no-header -fp " gensub(/.*\/([0-9]+).*/, "\\1", "g", a) "'" $fmt"'");
+	print "  -> AnonHugePages:",kb[a]/1024.0,"MB"
      }
 }' /proc/*/smaps
 exit 0
