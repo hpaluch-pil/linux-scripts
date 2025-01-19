fbe8d90e37ada8a3077321dc0b84d269e1881b6b
Henryk Paluch <henryk.paluch@pickering.cz>
2025-01-19 10:31:42 +0100

    Added egl-utils - for inxi reporting

--- gnome/my-gnome-fedora-pkgs.sh
+++ gnome/my-gnome-fedora-pkgs.sh
@@ -6,5 +6,5 @@ set -xeuo pipefail
 # d-feet: DBus GUI
 sudo dnf install gnome-terminal terminator gnome-tweaks gnome-extensions-app \
 	gnome-shell-extension-dash-to-panel gnome-shell-extension-system-monitor-applet \
-	d-feet terminus-fonts
+	d-feet terminus-fonts egl-utils
 exit 0
