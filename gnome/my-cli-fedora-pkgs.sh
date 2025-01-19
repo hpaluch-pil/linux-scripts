517f9e38e3b97158cbf4bef53bea055fd62cba9e
Henryk Paluch <henryk.paluch@pickering.cz>
2025-01-19 09:40:02 +0100

    Added my favorite CLI packages

---
+++ gnome/my-cli-fedora-pkgs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# my-cli-fedora-pkgs.sh - My favorite CLI packages in fedora
+# Disclaimer: Use on your own risk!
+set -xeuo pipefail
+
+# fastfetch - clone of neofetch, ncdu - disk usage utility
+sudo dnf install inxi fastfetch jq mc lynx ncdu
+exit 0
