45925c1e6883b27842bdb2daaf449f006498611a
Henryk Paluch <henryk.paluch@pickering.cz>
2025-02-28 09:21:54 +0100

    gdk-pixbuf2-modules-extra required for many programs to show icons properly

--- gnome/my-gnome-fedora-pkgs.sh
+++ gnome/my-gnome-fedora-pkgs.sh
@@ -4,7 +4,10 @@
 set -xeuo pipefail
 
 # d-feet: DBus GUI
+# gdk-pixbuf2-modules-extra required for icons in mtpaint,
+#   etc, see https://www.reddit.com/r/Fedora/comments/1gfildw/solved_fedora_41gnome_47_missing_icons_broken/?rdt=55345
 sudo dnf install gnome-terminal terminator gnome-tweaks gnome-extensions-app \
 	gnome-shell-extension-dash-to-panel gnome-shell-extension-system-monitor-applet \
-	d-feet terminus-fonts egl-utils
+	d-feet terminus-fonts egl-utils \
+	gdk-pixbuf2-modules-extra
 exit 0
